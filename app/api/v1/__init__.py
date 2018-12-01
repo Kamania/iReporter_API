977d9b1212f5c21f1a28f05a97c9cafd79043de5
joseph-kamania-chiira <kamaniajoseph@gmail.com>
2018-12-01 07:22:36 +0300

    [Finishes #162361664] Adds an endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,4 +1,4 @@
-from app.api.v1.view import UserReportRedFlagList
+from app.api.v1.view import UserReportRedFlagList, UserReportRedFlag
 from flask_restful import Api, Resource
 from flask import Blueprint
 
@@ -6,3 +6,4 @@ version_one = Blueprint('v1', __name__, url_prefix="/api/v1")
 
 api = Api(version_one)
 api.add_resource(UserReportRedFlagList, '/red_flag_records')
+api.add_resource(UserReportRedFlag, '/red_flag_record/<int:id>')

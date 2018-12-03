14d0b61d110433abd7927fffe9e87be6466f7271
joseph-kamania-chiira <kamaniajoseph@gmail.com>
2018-12-03 23:01:35 +0300

    [Finishes #162362149] Updated the init files

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,6 @@
-from app.api.v1.view import UserReportRedFlagList, UserReportRedFlag
-from flask_restful import Api, Resource
+from app.api.v1.view.record_view import UserReportRedFlagList, UserReportRedFlag
+from app.api.v1.view.user_view import Register
 from flask import Blueprint
 
 version_one = Blueprint('v1', __name__, url_prefix="/api/v1")
 
-api = Api(version_one)
-api.add_resource(UserReportRedFlagList, '/red_flag_records')
-api.add_resource(UserReportRedFlag, '/red_flag_record/<int:id>')

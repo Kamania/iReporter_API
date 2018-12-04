bde9e148ef7e8c3f171cc1f8a92a3eda28a61e92
joseph-kamania-chiira <kamaniajoseph@gmail.com>
2018-12-04 17:03:09 +0300

    [Finishes #162362149] Update the init file in app

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,9 @@ from app.api.v1.url import routes
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config['development'])
-
-    from app.api.v1 import version_one
-    api = Api(version_one)
+    app.config.from_object(app_config['development'])    
+    api = Api(v1)
+    '''register'''
     app.register_blueprint(v1)
-
     routes(api)
     return app

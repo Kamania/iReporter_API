e5619e62a5dff65ab4039ea9c8f26da95eaac86a
joseph-kamania-chiira <kamaniajoseph@gmail.com>
2018-12-11 17:42:09 +0300

    [Finishes #162490007] Updates travis, v1/init, url and requirements.txt

--- app/api/v1/url.py
+++ app/api/v1/url.py
@@ -1,7 +1,6 @@
-from . import Register, UserReportRedFlag, UserReportRedFlagList, version_one
+from . import UserReportRedFlag, UserReportRedFlagList, version_one
 from flask_restful import Api, Resource
 from .views import Patch_location, Patch_comment
-from .user_views import GetOneUser
 
 
 def routes(api):
@@ -10,7 +9,5 @@ def routes(api):
     api.add_resource(UserReportRedFlag, '/records/<id>')
     api.add_resource(Patch_location, '/records/<id>/location')
     api.add_resource(Patch_comment, '/records/<id>/comment')
-    api.add_resource(Register, '/user')
-    api.add_resource(GetOneUser, '/user/<id>')
 
     return None

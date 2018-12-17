1e5645455d43f7f33fb02b40e5836d7eecaba5c2
joseph-kamania-chiira <kamaniajoseph@gmail.com>
2018-12-17 04:34:00 +0300

    [Finishes #162628084] Get all Incidences

---
+++ app/db_config.py
@@ -0,0 +1,66 @@
+import psycopg2
+
+url = "dbname='ireporter' host='localhost' port='5432' user='postgres'\
+     password='root'"
+
+test_url = "dbname='ireporter_test' host='localhost' port='5432' user='postgres' password='root'"
+
+
+def connection(url):
+    # connect to database
+    print("connecting...")
+    conn = psycopg2.connect(url)
+    return conn
+
+
+def init_db():
+    conn = connection(url)
+    return conn
+    
+
+def init_test_db():
+    conn = connection(test_url)
+    return conn
+
+
+def create_tables():
+    conn = psycopg2.connect(url)
+    curr = conn.cursor()
+    queries = tables()
+
+    for query in queries:
+        curr.execute(query)
+    conn.commit()
+    return curr
+
+
+def tables():
+    users = """CREATE TABLE IF NOT EXISTS users(
+        user_id serial PRIMARY KEY NOT NULL,
+        fname varchar(255) NOT NULL,
+        lname varchar(255) NOT NULL,
+        othernames varchar(255),
+        email varchar(255) NOT NULL,
+        phoneNumber varchar(255) NOT NULL,
+        username varchar(255) NOT NULL,
+        registeredOn TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        isAdmin BOOLEAN NOT NULL,
+        password varchar(1000)
+    )"""
+
+    incidents = """CREATE TABLE IF NOT EXISTS incidents(
+        incident_id serial PRIMARY KEY NOT NULL,
+        createdOn TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        createdBy INTEGER NOT NULL,
+        type varchar(50) NOT NULL,
+        location varchar(1000) NOT NULL,
+        status varchar(50) NOT NULL,
+        comments varchar(1000)
+    )"""
+
+    queries = [users, incidents]
+    return queries
+
+
+if __name__ == "__main__":
+    init_db()

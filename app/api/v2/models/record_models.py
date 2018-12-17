18ada0a8c62d49187fadd78a65f51c477253f52c
joseph-kamania-chiira <kamaniajoseph@gmail.com>
2018-12-17 04:21:23 +0300

    [Finishes #162627918] Post a record

---
+++ app/api/v2/models/record_models.py
@@ -0,0 +1,91 @@
+from app.db_config import connection, url, init_db
+from psycopg2.extras import RealDictCursor
+
+
+class RaiseRedFlagModel():
+
+    def __init__(self):
+        self.conn = init_db()
+        self.curr = self.conn.cursor(cursor_factory=RealDictCursor)
+
+    def save(self, user_id, type, location, status, comments):
+
+        payload = {            
+            "createdBy": user_id,
+            "type": type,
+            "location": location,
+            "status": status,
+            "comments": comments
+        }
+        query = """INSERT INTO incidents(createdBy, type, location, status, comments) VALUES('{0}', '{1}', '{2}', '{3}', '{4}');""".format(payload["createdBy"], payload['type'], payload['location'], payload['status'], payload['comments'])
+        self.curr.execute(query)
+        self.conn.commit()
+
+    def get_redFlag(self):
+        query = '''SELECT * FROM incidents'''
+        curr = self.curr
+        curr.execute(query)
+        records = curr.fetchall()
+        if not records:
+            return None
+        return records
+    
+    def get_record_by_id(self, id):
+        query = """SELECT * FROM incidents WHERE incident_id=%s"""
+        curr = self.curr
+        curr.execute(query, (id,))
+        record = curr.fetchone()
+        if not record:
+            return None
+        return record
+
+    def del_record(self, id):
+        query = """DELETE FROM incidents  where incident_id=%s"""
+        curr = self.curr
+        curr.execute(query, (id,))
+        self.conn.commit()
+
+    @staticmethod
+    def check_record(records):
+        result = []
+        if not records:
+            return {
+                'message': 'No record(s) found'
+            }
+        for record in records:
+            record['createdon'] = record['createdon'].strftime('%A %d. %B %Y')
+            result.append(record)
+        return result
+
+    def find(self, id):
+        query = '''SELECT * FROM incidents where incident_id= %s'''
+        curr = self.curr
+        curr.execute(query, (id,))
+        record = curr.fetchall()
+        return record
+
+    def update_location(self, location, id):
+        query = """UPDATE incidents SET location=%s WHERE  incident_id=%s"""
+        curr = self.curr
+        curr.execute(query, (location, id,))
+        self.conn.commit()
+
+    def update_comment(self, comment, id):
+        query = """UPDATE incidents SET comments=%s WHERE  incident_id=%s"""
+        curr = self.curr
+        curr.execute(query, (comment, id,))
+        self.conn.commit()
+
+    def update_status(self, status, id):
+        query = """UPDATE incidents SET status=%s WHERE  incident_id=%s"""
+        curr = self.curr
+        curr.execute(query, (status, id,))
+        self.conn.commit()
+
+    def check_isAdmin(self, id):
+        query = """SELECT * FROM users WHERE user_id=%s"""
+        curr = self.curr
+        curr.execute(query, (id,))
+        user = curr.fetchone()
+        return user
+        
